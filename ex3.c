4d25541b9af93da58e58e8245485a3d9ac3dd933
s1240107 <s1240107@stdrsv6.u-aizu.ac.jp>
2018-07-02 16:43:37 +0900

    Initial revision

---
+++ ex3.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+
+int main(){
+  srand(time(NULL));
+  
+  int i, a[2], sum=0;
+  
+  printf("Rolling the dice...\n");
+ 
+  
+  for(i = 0; i < 2; i++) {
+    a[i] = rand() % 6 + 1;
+    printf("Die %d: %d\n", i + 1, a[i]);
+    sum += a[i];
+  }
+ 
+  printf("Total value: %d\n", sum);
+  
+  
+  return 0;
+}
